5c4900272bd04c1d559d570daa1c11723020dfbe
Max Eisner <4730112+max-ae@users.noreply.github.com>
2024-01-13 14:00:18 +0100

    cleanup

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'SimpleRegs';
 
 export const SITE_DESCRIPTION = 'SimpleRegs is a platform for easing regulatory navigation for energy communities.';
